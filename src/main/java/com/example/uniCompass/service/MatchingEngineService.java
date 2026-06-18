c95e2ddb181631589c8aad1e8106375fa90c43c0
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-19 03:40:56 +0400

    create method that should calculate the percentage of how registered user matches the university requirements

--- src/main/java/com/example/uniCompass/service/MatchingEngineService.java
+++ src/main/java/com/example/uniCompass/service/MatchingEngineService.java
@@ -1,7 +1,14 @@
 package com.example.uniCompass.service;
 
+import com.example.uniCompass.dto.request.UserProfileRequest;
+import com.example.uniCompass.dto.response.UniversityMatchResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MatchingEngineService {
+    public UniversityMatchResponse calculateMatchPercentage(String email, UserProfileRequest userProfile) {
+        return null;
+        // TODO: algoritmia dasaweri romelic gamoitvlis procents
+    }
 }
