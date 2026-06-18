e2896dbd86953633e97e87a748e4d42ea0a6e369
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-19 03:34:00 +0400

    made AppUser.java implement UserDetails

--- src/main/java/com/example/uniCompass/model/AppUser.java
+++ src/main/java/com/example/uniCompass/model/AppUser.java
@@ -1,8 +1,14 @@
 package com.example.uniCompass.model;
 
+import com.example.uniCompass.roles.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor
@@ -12,7 +18,7 @@ import java.util.List;
 @ToString
 @Entity
 @Table(name = "app_users")
-public class AppUser {
+public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -42,4 +48,39 @@ public class AppUser {
             inverseJoinColumns = @JoinColumn(name = "university_id")
     )
     private List<University> favoriteUniversities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(UserRole.USER_ROLE.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.passwordHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
