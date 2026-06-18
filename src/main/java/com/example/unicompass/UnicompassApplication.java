50e0d2058cf36256aa4d8bc8f60e014680e57afb
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-18 15:38:35 +0400

    create spring boot application

---
+++ src/main/java/com/example/unicompass/UnicompassApplication.java
@@ -0,0 +1,13 @@
+package com.example.unicompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicompassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unicompass/UnicompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unicompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
