d70de507fe1276bf7a22721a588d577d1fa64532
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-18 22:48:07 +0400

    write UserChecklist.java

---
+++ src/main/java/com/example/uniCompass/model/UserChecklist.java
@@ -0,0 +1,27 @@
+package com.example.uniCompass.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user_checklists")
+public class UserChecklist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser user;
+
+    @Column(name = "task_text", nullable = false)
+    private String taskText;
+
+    @Column(name = "is_completed")
+    private Boolean isCompleted = false;
+}
