690e9dcee5b8fa9687f9dc69dd8504edfd9e2b1a
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-22 02:00:55 +0400

    add findUser method in UserProfileService.java

--- src/main/java/com/example/uniCompass/service/UserProfileService.java
+++ src/main/java/com/example/uniCompass/service/UserProfileService.java
@@ -1,13 +1,26 @@
 package com.example.uniCompass.service;
 
+import com.example.uniCompass.dto.response.UserProfileResponse;
+import com.example.uniCompass.model.AppUser;
+import com.example.uniCompass.model.UserProfile;
 import com.example.uniCompass.repository.UserProfileRepository;
+import com.example.uniCompass.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserProfileService {
-    private final UserProfileRepository repository;
+    private final UserProfileRepository userProfileRepository;
+    private final UserRepository userRepository;
 
-    public UserProfileService(UserProfileRepository repository) {
-        this.repository = repository;
+    public UserProfileService(UserProfileRepository userProfileRepository, UserRepository userRepository) {
+        this.userProfileRepository = userProfileRepository;
+        this.userRepository = userRepository;
+    }
+
+    public UserProfileResponse findUser(String email) {
+        AppUser currentUser = userRepository.findByEmail(email).orElseThrow();
+        UserProfile profile = currentUser.getProfile();
+
+        return new UserProfileResponse(currentUser, profile);
     }
 }
