1f297bf74797a9bb1eff334117c2aa4fc1337333
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-22 01:56:50 +0400

    add check-availability method for better user-friendliness and better performance

--- src/main/java/com/example/uniCompass/controller/AuthController.java
+++ src/main/java/com/example/uniCompass/controller/AuthController.java
@@ -1,16 +1,16 @@
 package com.example.uniCompass.controller;
 
+import com.example.uniCompass.dto.request.FullRegistrationRequest;
 import com.example.uniCompass.dto.response.AuthResponse;
 import com.example.uniCompass.dto.request.LoginRequest;
-import com.example.uniCompass.dto.request.RegisterRequest;
 import com.example.uniCompass.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,7 +19,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest userDto) {
+    public ResponseEntity<String> register(@RequestBody FullRegistrationRequest userDto) {
         authService.register(userDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
     }
@@ -27,6 +27,21 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest userDto) {
         AuthResponse response = authService.login(userDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/check-availability")
+    public ResponseEntity<Map<String, Boolean>> checkAvailability(
+            @RequestParam String email,
+            @RequestParam String username) {
+
+        boolean emailTaken = authService.existsByEmail(email);
+        boolean usernameTaken = authService.existsByUsername(username);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("emailTaken", emailTaken);
+        response.put("usernameTaken", usernameTaken);
+
         return ResponseEntity.ok(response);
     }
 }
