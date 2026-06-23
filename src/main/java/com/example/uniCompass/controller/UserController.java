9a1355b01b18ef3df957aecbc3d2208e40034f7f
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-23 16:04:08 +0400

    write put methods in user controller

--- src/main/java/com/example/uniCompass/controller/UserController.java
+++ src/main/java/com/example/uniCompass/controller/UserController.java
@@ -20,4 +20,31 @@ public class UserController {
     public ResponseEntity<UserProfileResponse> findUser(@AuthenticationPrincipal UserDetails currentUser) {
         return ResponseEntity.ok(service.findUser(currentUser.getUsername()));
     }
+
+    @PostMapping("/user/update-basics")
+    public ResponseEntity<String> updateBasics(
+            @AuthenticationPrincipal UserDetails currentUser,
+            @RequestBody BasicsUpdateRequest request
+    ) {
+        service.updateBasics(currentUser.getUsername(), request);
+        return ResponseEntity.ok("Basics updated successfully");
+    }
+
+    @PostMapping("/user/update-academics")
+    public ResponseEntity<String> updateAcademics(
+            @AuthenticationPrincipal UserDetails currentUser,
+            @RequestBody AcademicsUpdateRequest request
+    ) {
+        service.updateAcademics(currentUser.getUsername(), request);
+        return ResponseEntity.ok("Academics updated successfully");
+    }
+
+    @PostMapping("/user/update-preferences")
+    public ResponseEntity<String> updatePreferences(
+            @AuthenticationPrincipal UserDetails currentUser,
+            @RequestBody PreferencesUpdateRequest request
+    ) {
+        service.updatePreferences(currentUser.getUsername(), request);
+        return ResponseEntity.ok("Preferences updated successfully");
+    }
 }
