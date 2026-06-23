f5b4f0972fa908331945b036c41f87a043da0a96
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-23 14:21:15 +0400

    simplified imports

--- src/main/java/com/example/uniCompass/controller/UserController.java
+++ src/main/java/com/example/uniCompass/controller/UserController.java
@@ -5,9 +5,7 @@ import com.example.uniCompass.service.UserProfileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
