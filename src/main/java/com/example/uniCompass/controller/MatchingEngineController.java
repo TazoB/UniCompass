9b2e025a6f3c1c5b5bdd4bdeb94048273cd50128
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-24 16:01:36 +0400

    create two separate methods, one of them is called when the whole map is load and the js has to insert pins and their colors. the other one is called when the particular university pin is clicked

--- src/main/java/com/example/uniCompass/controller/MatchingEngineController.java
+++ src/main/java/com/example/uniCompass/controller/MatchingEngineController.java
@@ -1,18 +1,18 @@
 package com.example.uniCompass.controller;
 
-import com.example.uniCompass.dto.request.UserProfileRequest;
+import com.example.uniCompass.dto.response.PersonalizedPinResponse;
 import com.example.uniCompass.dto.response.UniversityMatchResponse;
+import com.example.uniCompass.dto.response.UniversityMatchesForPins;
 import com.example.uniCompass.service.MatchingEngineService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/match")
+@RequestMapping("/api/universities")
 public class MatchingEngineController {
     private final MatchingEngineService service;
 
@@ -20,11 +20,19 @@ public class MatchingEngineController {
         this.service = service;
     }
 
-    @PostMapping("/calculate")
+    @PostMapping("/calculate/{id}")
     public ResponseEntity<UniversityMatchResponse> calculate(
             @AuthenticationPrincipal UserDetails currentUser,
-            @RequestBody UserProfileRequest userProfile
-            ) {
-        return ResponseEntity.ok(service.calculateMatchPercentage(currentUser.getUsername(), userProfile));
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(service.universityMatch(currentUser.getUsername(), id));
     }
+
+    @GetMapping("/map-matches")
+    public ResponseEntity<List<PersonalizedPinResponse>> pinMatches(
+            @AuthenticationPrincipal UserDetails currentUser
+    ) {
+        return ResponseEntity.ok(service.matchPins(currentUser.getUsername()));
+    }
+
 }
