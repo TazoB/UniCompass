e196f08f89bc1da424e90d9285a1aecaa5aa1ce4
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-06-27 22:19:20 +0400

    add basic endpoints for modifying and deleting user data

--- src/main/java/com/example/uniCompass/controller/UserController.java
+++ src/main/java/com/example/uniCompass/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.example.uniCompass.controller;
 
-import com.example.uniCompass.dto.request.AcademicsUpdateRequest;
-import com.example.uniCompass.dto.request.BasicsUpdateRequest;
-import com.example.uniCompass.dto.request.PreferencesUpdateRequest;
+import com.example.uniCompass.dto.request.*;
 import com.example.uniCompass.dto.response.UserProfileResponse;
 import com.example.uniCompass.service.UserProfileService;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -24,7 +25,7 @@ public class UserController {
         return ResponseEntity.ok(service.findUser(currentUser.getUsername()));
     }
 
-    @PostMapping("/user/update-basics")
+    @PutMapping("/user/update-basics")
     public ResponseEntity<String> updateBasics(
             @AuthenticationPrincipal UserDetails currentUser,
             @RequestBody BasicsUpdateRequest request
@@ -33,7 +34,7 @@ public class UserController {
         return ResponseEntity.ok("Basics updated successfully");
     }
 
-    @PostMapping("/user/update-academics")
+    @PutMapping("/user/update-academics")
     public ResponseEntity<String> updateAcademics(
             @AuthenticationPrincipal UserDetails currentUser,
             @RequestBody AcademicsUpdateRequest request
@@ -42,12 +43,48 @@ public class UserController {
         return ResponseEntity.ok("Academics updated successfully");
     }
 
-    @PostMapping("/user/update-preferences")
+    @PutMapping("/user/update-preferences")
     public ResponseEntity<String> updatePreferences(
             @AuthenticationPrincipal UserDetails currentUser,
             @RequestBody PreferencesUpdateRequest request
     ) {
         service.updatePreferences(currentUser.getUsername(), request);
         return ResponseEntity.ok("Preferences updated successfully");
+    }
+
+    @PutMapping("/user/update-extracurriculars")
+    public ResponseEntity<String> updateExtracurriculars(
+            @AuthenticationPrincipal UserDetails currentUser,
+            @RequestBody List<String> extracurriculars
+    ) {
+        service.updateExtracurriculars(currentUser.getUsername(), extracurriculars);
+        return ResponseEntity.ok("Extracurriculars updated successfully");
+    }
+
+    @PutMapping("/user/update-skills")
+    public ResponseEntity<String> updateSkills(
+            @AuthenticationPrincipal UserDetails currentUser,
+            @RequestBody List<String> skills
+    ) {
+        service.updateSkills(currentUser.getUsername(), skills);
+        return ResponseEntity.ok("Skills updated successfully");
+    }
+
+    @PutMapping("/user/update-languages")
+    public ResponseEntity<String> updateLanguages(
+            @AuthenticationPrincipal UserDetails currentUser,
+            @RequestBody List<LanguageDTO> languages
+    ) {
+        service.updateLanguages(currentUser.getUsername(), languages);
+        return ResponseEntity.ok("Languages updated successfully");
+    }
+
+    @PutMapping("/user/update-checklist")
+    public ResponseEntity<String> updateChecklist(
+            @AuthenticationPrincipal UserDetails currentUser,
+            @RequestBody List<ChecklistDTO> checklistItems
+    ) {
+        service.updateChecklist(currentUser.getUsername(), checklistItems);
+        return ResponseEntity.ok("Checklist updated successfully");
     }
 }
