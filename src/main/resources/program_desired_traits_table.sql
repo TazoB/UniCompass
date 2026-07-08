9a8419531e9c0b2e7b4017b75f1f744cf00ad6d8
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-07-08 18:12:37 +0400

    correct the syntax

--- src/main/resources/program_desired_traits_table.sql
+++ src/main/resources/program_desired_traits_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE program_desired_traits IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS program_desired_traits (
     program_id BIGINT NOT NULL,
     trait VARCHAR(255),
     CONSTRAINT fk_program FOREIGN KEY (program_id) REFERENCES programs(id) ON DELETE CASCADE
